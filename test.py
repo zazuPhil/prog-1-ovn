bbaf297badfd6836521bff5e2d546f4d37ab3e8e
zazuPhil <pichayut.hongthai@elev.ga.ntig.se>
2020-08-31 14:45:05 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+v = 5
+print(v * v)
+namn = input('Vad heter du?')
+surnamn = input( 'efternamn: ')
+
+print('Hej', namn, ' ', surnamn)
+
+print ( 2847589348 + 93743247 * 34 / 12)
+
+tal1 = input('skriv ett tal1? ')
+
+print(tal1 + 4578342865)
+x = int
